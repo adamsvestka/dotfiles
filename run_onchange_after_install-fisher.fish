149700e8f1793a7da391780b0ebe5aabde32fecc
Adam Švestka <svestka.adam1@gmail.com>
2026-09-19 00:36:54 +0200

    fastfetch

--- run_onchange_after_install-fisher.fish
+++ run_onchange_after_install-fisher.fish
@@ -10,3 +10,5 @@ end
 fisher update
 
 tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Round --powerline_prompt_heads=Round --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character' --prompt_connection=Solid --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Darkest --prompt_spacing=Sparse --icons='Many icons' --transient=No
+
+bind --preset alt-backspace backward-kill-word
