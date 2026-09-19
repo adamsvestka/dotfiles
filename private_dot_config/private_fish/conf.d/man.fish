879bbe2b3b0d29121de175fec1adbc7c8fc9d5a7
Adam Švestka <svestka.adam1@gmail.com>
2026-09-19 17:01:02 +0200

    fish

---
+++ private_dot_config/private_fish/conf.d/man.fish
@@ -0,0 +1,11 @@
+set -x LESS_TERMCAP_mb (set_color --bold magenta)
+set -x LESS_TERMCAP_md (set_color --bold blue)
+set -x LESS_TERMCAP_me (set_color normal)
+set -x LESS_TERMCAP_se (set_color normal)
+set -x LESS_TERMCAP_so (set_color --bold black --background white)
+set -x LESS_TERMCAP_ue (set_color normal)
+set -x LESS_TERMCAP_us (set_color --bold --underline green)
+set -x LESS_TERMCAP_mr (set_color --reverse)
+set -x LESS_TERMCAP_mh (set_color --dim)
+set -x MANPAGER less
+set -x MANROFFOPT -c
